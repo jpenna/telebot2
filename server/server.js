254469b208c431c12bae7b5f1aef95a33c00e994
Juliano Penna <julianopenna@gmail.com>
2017-01-16 18:03:30 -0200

    Add support to bot using Botgram lib

--- server/server.js
+++ server/server.js
@@ -5,11 +5,11 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+const bot = require('./bot/bot');
+
 server.listen(3100);
 
 app.use(express.static(path.join(__dirname, '../public')));
-
-console.log(path.join(__dirname + '../public'));
 
 io.on('connection', (socket) => {
   socket.on('newMessage', (data, callback) => {
