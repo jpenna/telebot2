2cec4f7930e6d50ae4142967a3f2bbdd6291a66b
Juliano Penna <julianopenna@gmail.com>
2017-01-16 14:34:47 -0200

    Add websocket on client

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
-const app = require('express')();
+const express = require('express');
+const path = require('path');
+
+const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 server.listen(3100);
 
-app.get('/', (req, res) => {
-  res.send('oi');
-});
+app.use(express.static(path.join(__dirname, '../public')));
+
+console.log(path.join(__dirname + '../public'));
 
 io.on('connection', (socket) => {
-
+  socket.on('newMessage', (data, callback) => {
+    console.log(data);
+    callback();
+  });
 });
