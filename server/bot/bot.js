14525a90021713a848e101b036f6af6c16124af2
Juliano Penna <julianopenna@gmail.com>
2017-01-16 23:09:28 -0200

    Create db utils to insert messages and chats

--- server/bot/bot.js
+++ server/bot/bot.js
@@ -3,22 +3,38 @@
 const {bot} = require('./connection/connect');
 const web = require('../web/web');
 const botReply = require('./botReply');
+const db = require('../utils/db');
 
 // handles /start command
 bot.command("start", (msg, reply, next) => {
   console.log(msg);
-  web.newChat(msg.chat);
+  const chat = msg.chat;
+
+  web.newChat(chat);
   botReply.send(reply, 'Welcome!');
+
+  db.insertChat({
+    chat_id: chat.id,
+    type: chat.type,
+    firstname: chat.firstname,
+    lastname: chat.lastname
+  });
 });
 
 // handles text messages
 bot.text((msg, reply, next) => {
+  console.log(msg);
+
+  const chat = msg.chat;
 
   const data = {
+    chat_id: chat.id,
+    author: chat.firstname,
     message: msg.text,
-    author: msg.chat.name
+    sentAt: new Date().getTime()
   }
 
+  db.insertMessage(data);
   web.sendMessage(data);
   botReply.send(reply, 'Hello!');
 });
---
+++ server/utils/db.js
@@ -0,0 +1,33 @@
+const {Chat} = require('../db/model/chat');
+const {Message} = require('../db/model/message');
+const _ = require('lodash');
+
+const db = {
+  insertChat (data) {
+
+    const chatData = _.pick(data, ['chat_id', 'type', 'firstname', 'lastname']);
+
+    const chat = new Chat(chatData);
+    chat.save(function (err) {
+      if (err) return console.log(err);
+      console.log('chat saved');
+    });
+  },
+
+  insertMessage(data) {
+    const chatId = data.chat_id;
+    const messageData = _.pick(data, ['author', 'message', 'sentAt']);
+
+    const message = new Message(messageData);
+
+    Chat.findOneAndUpdate({ chat_id: chatId },
+      { $push: { messages: message }}, { upsert: true },
+      (err) => {
+        if (err)
+          console.log(err)
+      });
+  }
+
+}
+
+module.exports = db;
