d9e2c168c7209d5b6a5de67d5560d266f62c8bb6
Juliano Penna <julianopenna@gmail.com>
2017-01-22 18:56:35 -0200

    Add bot utils tests

---
+++ server/test/bot/botReply.test.js
@@ -0,0 +1,60 @@
+/* eslint no-undef:off */
+
+require('../../config/config');
+const expect = require('expect');
+const chatSeed = require('../seed/chatSeed');
+const botReply = require('../../bot/botReply');
+const web = require('../../web/web');
+const { Chat } = require('../../db/model/chat');
+
+describe('botReply', () => {
+  describe('send()', () => {
+
+    before(() => {
+      // Set variables
+      reply = { text(x) {} };
+      botAPIMsg = { chat: { id: chatSeed.chat1.chatId } };
+      msg = 'Something';
+
+      author = 'Telebot';
+      type = 'bot';
+      chatId = botAPIMsg.chat.id;
+      message = msg;
+
+      // Set spies
+      replySpy = expect.spyOn(reply, 'text');
+      sendMsgSpy = expect.spyOn(web, 'sendMessage');
+    });
+
+    beforeEach((done) => {
+      // Populate chats to insert message
+      chatSeed.populateChats().then(() => {
+        // Execute send()
+        botReply.send(reply, botAPIMsg, msg);
+        done();
+      });
+    });
+
+    it('should reply client with msg param', () => {
+      expect(replySpy).toHaveBeenCalled();
+      expect(replySpy).toHaveBeenCalledWith(msg);
+    });
+
+    it('should send message to web', () => {
+      expect(sendMsgSpy).toHaveBeenCalled();
+      expect(sendMsgSpy.calls[0].arguments).toInclude(chatId, type, author, message);
+    });
+
+    it('should save message in db', (done) => {
+      setTimeout(() => {
+        Chat.findOne({ chatId }).then((chat) => {
+          expect(chat).toExist();
+          expect(chat.messages.length).toBe(3);
+          expect(chat.messages[2].message).toBe(msg);
+          done();
+        });
+      }, 300);
+    });
+
+  });
+});
---
+++ server/test/bot/utils.test.js
@@ -0,0 +1,74 @@
+/* eslint no-undef:off */
+
+require('../../config/config');
+const expect = require('expect');
+const https = require('https');
+const fs = require('fs');
+// const { bot } = require('../../bot/bot');
+// const chatSeed = require('../seed/chatSeed');
+const utils = require('../../bot/utils');
+// const botgramSeed = require('../seed/botgramSeed');
+// const botReply = require('../../bot/botReply');
+// const web = require('../../web/web');
+// const { Chat } = require('../../db/model/chat');
+const axios = require('axios');
+
+describe.only('utils', () => {
+
+  before(() => {
+    // Set variables
+    fileId = 123;
+    filePath = 'xyz';
+    dir = './public/img/avatars';
+    get1 = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/getUserProfilePhotos`;
+    get2 = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/getFile`;
+    get3 = `https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${filePath}`;
+    userId = 1;
+
+    // Set spies
+    fsSpy = expect.spyOn(fs, 'createWriteStream');
+    httpsGetSpy = expect.spyOn(https, 'get');
+    axiosGetSpy = expect.spyOn(axios, 'get').andReturn(
+      new Promise((resolve) => {
+        resolve({
+          data: {
+            ok: true,
+            result: {
+              photos: [
+                [{ file_id: fileId }]
+              ],
+              file_path: filePath
+            }
+          }
+        });
+      })
+    );
+
+    // Call getUserAvatar()
+    utils.getUserAvatar(userId);
+  });
+
+  describe.only('getUserAvatar()', () => {
+    it('should get /getUserProfilePhotos', () => {
+      expect(axiosGetSpy).toHaveBeenCalled();
+      expect(axiosGetSpy.calls[0].arguments).toInclude(get1)
+        .toInclude({ params: { user_id: userId } });
+    });
+
+    it('should get /getFile', () => {
+      expect(axiosGetSpy.calls[1].arguments).toInclude(get2)
+        .toInclude({ params: { file_id: fileId } });
+    });
+
+    it('should create write stream to /public/img/avatars', () => {
+      expect(fsSpy).toHaveBeenCalled();
+      expect(fsSpy).toHaveBeenCalledWith(`${dir}/${userId}.jpg`);
+    });
+
+    it('should request /file/*/file_path', () => {
+      expect(httpsGetSpy).toHaveBeenCalled();
+      expect(httpsGetSpy.calls[0].arguments).toInclude(get3);
+    });
+
+  });
+});
